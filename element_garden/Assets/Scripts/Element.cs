6d309969a525303f69f3cc0bd25561b6ebf81832
whatPanache <pwcarney@bu.edu>
2017-06-27 19:26:42 -0400

    Fuck angles

--- element_garden/Assets/Scripts/Element.cs
+++ element_garden/Assets/Scripts/Element.cs
@@ -10,9 +10,6 @@ public abstract class Element : MonoBehaviour
 
     Image button_image;
     Camera cam;
-    Vector3 cam_pos;
-    Vector3 screen_center_pos;
-    float screen_to_camera_dist;
 
     bool isHeld = false;
 
@@ -21,19 +18,9 @@ public abstract class Element : MonoBehaviour
         button_image = button.GetComponent<Image>();
 
         cam = Camera.main;
-
-        cam_pos = cam.transform.position;
-
-        screen_center_pos = cam.ScreenToWorldPoint(
-            new Vector3(
-                Screen.width / 2f,
-                Screen.height / 2f,
-                cam.nearClipPlane));
-
-        screen_to_camera_dist = Vector3.Distance(cam_pos, screen_center_pos);
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // Change button color
         if (isHeld)
@@ -44,34 +31,21 @@ public abstract class Element : MonoBehaviour
         // Execute functionality
         if (Input.GetMouseButton(0) && isHeld)
         {
-            Execute( GetMouseFieldPosition() );
+            //create a ray cast and set it to the mouses cursor position in game
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50f))
+            {
+                //draw invisible ray cast/vector
+                Debug.DrawLine(ray.origin, hit.point);
+                Debug.Log(hit.point);
+                Execute(hit.point);
+            }
         }
     }
 
     // Needs to be implemented in the specific element classes
     protected abstract void Execute(Vector3 inputVec);
-
-    public Vector3 GetMouseFieldPosition()
-    {
-        Vector3 mousePos = cam.ScreenToWorldPoint(
-            new Vector3(
-                Input.mousePosition.x,
-                Input.mousePosition.y,
-                cam.nearClipPlane));
-
-        float x_dist = mousePos.x - screen_center_pos.x;
-        float y_dist = mousePos.y - screen_center_pos.y;
-
-        float x_angle = (float)Math.Atan2(x_dist, screen_to_camera_dist);
-        float y_angle = (float)Math.Atan2(y_dist, screen_to_camera_dist);
-
-        float x = (float)Math.Tan(Math.PI - (cam.transform.eulerAngles.y * Math.PI / 180f) + x_angle) * mousePos.x + mousePos.x;
-        float z = (float)Math.Tan(Math.PI - (cam.transform.eulerAngles.x * Math.PI / 180f) + y_angle) * mousePos.z + mousePos.z;
-        float y = 0.5f;
-
-        Debug.Log(new Vector3(x, y, z));
-        return new Vector3(x, y, z);
-    }
 
     public void Holding ()
     {
