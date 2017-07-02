98548f6c4f152eccb5772c050a034371f702ab81
whatPanache <pwcarney@bu.edu>
2017-07-02 09:56:46 -0400

    No seeds on side of blocks

--- element_garden/Assets/Scripts/Dirt.cs
+++ element_garden/Assets/Scripts/Dirt.cs
@@ -16,7 +16,11 @@ public class Dirt : Element
             {
                 thing.GetComponent<Ground>().has_seed = true;
 
-                Instantiate(seed_prefab, inputPos, Quaternion.identity);
+                // We don't want seeds on the sides of blocks. Force to top.
+                Vector3 potential_position = new Vector3(inputPos.x, thing.transform.localScale.y / 2f, inputPos.z);
+
+                GameObject seed = Instantiate(seed_prefab, potential_position, Quaternion.identity);
+                seed.transform.Rotate(0f, Random.Range(0f, 360f), 0f);
             }
         }
     }
