8bfa9b7db5b4c385a619b7604ca886b235086ce3
whatPanache <pwcarney@bu.edu>
2017-07-05 19:14:48 -0400

    Janky birds

---
+++ element_garden/Assets/Scripts/Bird.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float bird_speed;
+    public float min_dist_to_plant;
+
+    GameObject target;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // TODO: Make "plant" maybe
+        GameObject[] potential_targets = GameObject.FindGameObjectsWithTag("Seed");
+
+        if (potential_targets.Length == 0)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+            
+        target = potential_targets[Random.Range(0, potential_targets.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > min_dist_to_plant)
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, bird_speed * Time.deltaTime);
+    }
+}
---
+++ element_garden/Assets/Scripts/BirdController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+    public int bird_cooldown;
+    public GameObject bird_prefab;
+
+    float last_bird = 0f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Time.timeSinceLevelLoad > last_bird)
+        {
+            last_bird += Time.timeSinceLevelLoad + bird_cooldown;
+
+            float x_spawn = Random.Range(-3.5f, 3.5f);
+            float z_spawn = Random.Range(-3.5f, 3.5f);
+            Instantiate(bird_prefab, new Vector3(x_spawn, 5f, z_spawn), Quaternion.identity);
+        }
+	}
+}
